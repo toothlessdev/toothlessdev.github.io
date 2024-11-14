070541a4714c0117496f31f8b5ff292d8df35971
toothlessdev <toothless042@gmail.com>
2024-11-15 04:09:32 +0900

    chore: add skeleton code for about, project page

---
+++ src/components/displays/Hero.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Card } from "../ui/card";
+
+export const Hero = () => {
+    return <Card className="p-6 bg-[#0e1117] border-[#3d444d] text-white rounded-[8px]">Hero</Card>;
+};
---
+++ src/pages/projects.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { RootLayout } from "@/layouts/RootLayout";
+
+export default function ProjectsPage() {
+    return <RootLayout>projects</RootLayout>;
+}
