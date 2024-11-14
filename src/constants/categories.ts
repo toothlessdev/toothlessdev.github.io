65e8d50c70b300f1a571b21a7f2a9c1f26f0b8c7
toothlessdev <toothless042@gmail.com>
2024-11-15 04:07:16 +0900

    chore: add nav, category constants

---
+++ src/constants/categories.ts
@@ -0,0 +1,7 @@
+export const categoryColor = {
+    React: "#67dafb",
+    JavaScript: "#f7e029",
+    TypeScript: "#2f73bf",
+};
+
+export type CategoryKey = keyof typeof categoryColor;
---
+++ src/constants/nav.ts
@@ -0,0 +1,26 @@
+export const nav = [
+    {
+        to: "/",
+        children: "Home",
+    },
+    {
+        to: "/about",
+        children: "About",
+    },
+    {
+        to: "/posts",
+        children: "Posts",
+    },
+    {
+        to: "/notes",
+        children: "Notes",
+    },
+    {
+        to: "/projects",
+        children: "Projects",
+    },
+    {
+        to: "/contact",
+        children: "Contact",
+    },
+];
