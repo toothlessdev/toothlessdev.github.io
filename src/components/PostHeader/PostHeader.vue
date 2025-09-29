3edad6b826adfc206270593eb632b7746782efc3
toothlessdev <toothless042@gmail.com>
2025-09-29 11:28:10 +0900

    feat: PostHeader 추가 및 레이아웃 적용

---
+++ src/components/PostHeader/PostHeader.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { useData } from "vitepress";
+import { computed } from "vue";
+import { formatDate } from "@/utils/date";
+
+const { frontmatter } = useData();
+
+const title = computed(() => frontmatter.value.title || "");
+const createdAt = computed(() => frontmatter.value.createdAt || "");
+const category = computed(() => frontmatter.value.category || "");
+const description = computed(() => frontmatter.value.description || "");
+</script>
+
+<template>
+    <header class="post-header">
+        <h1 class="post-title">{{ title }}</h1>
+
+        <div class="post-meta">
+            <span v-if="category" class="category-tag">{{ category }}</span>
+            <time v-if="createdAt" class="created-date">{{ formatDate(createdAt) }}</time>
+        </div>
+    </header>
+</template>
+
+<style>
+@import "./PostHeader.css";
+</style>
--- src/components/PostLayout/PostLayout.vue
+++ src/components/PostLayout/PostLayout.vue
@@ -2,15 +2,21 @@
 import { useData } from "vitepress";
 import DefaultTheme from "vitepress/theme";
 import PostComment from "@/components/PostComment/PostComment.vue";
+import PostHeader from "@/components/PostHeader/PostHeader.vue";
 const { Layout } = DefaultTheme;
 
 const { frontmatter } = useData();
 
+const showHeader = Boolean(frontmatter.value.title);
 const showComments = frontmatter.value.comment === true;
 </script>
 
 <template>
     <Layout>
+        <template #doc-before>
+            <PostHeader v-if="showHeader" />
+        </template>
+
         <template #doc-after>
             <PostComment v-if="showComments" />
         </template>
