dab83f1088c7de9b91ea77f55ab6cb7336590acb
toothlessdev <toothless042@gmail.com>
2025-11-03 06:43:18 +0900

    fix: computed 를 사용해서 showHeader 값을 reactive 하게 가져오도록 수정

--- src/components/PostLayout/PostLayout.vue
+++ src/components/PostLayout/PostLayout.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { useData } from "vitepress";
+import { computed } from "vue";
 import DefaultTheme from "vitepress/theme";
 import PostComment from "@/components/PostComment/PostComment.vue";
 import PostHeader from "@/components/PostHeader/PostHeader.vue";
@@ -7,8 +8,8 @@ const { Layout } = DefaultTheme;
 
 const { frontmatter } = useData();
 
-const showHeader = Boolean(frontmatter.value.title);
-const showComments = frontmatter.value.comment === true;
+const showHeader = computed(() => Boolean(frontmatter.value.title));
+const showComments = computed(() => frontmatter.value.comment === true);
 </script>
 
 <template>
